1c684ba8dd66dfda627a9a236a75e97be77e6dbe
u4235670606-cyber <u4235670606@gmail.com>
2026-02-03 17:41:54 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const settings = {
   timeZone: 'Asia/Karachi',
   botName: "MEGA-MD", // your bot name
   botOwner: 'Qasim Ali', // set your name
-  ownerNumber: '923051391007', // Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '491634524682', // Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20,
